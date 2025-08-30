4070b33f910aca788302761f818cd784940ab372
Tushar <tusharanshu18@gmail.com>
2025-08-30 08:56:08 +0530

    added roles jwt based authentication

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    ROLE_CHOICES =(
+        ("candidate", "Candidate"),
+        ("recruiter","Recruiter"),
+        ('user', 'User'),
+        ("admin", "Admin"),
+
+
+    )
+
+    role = models.CharField(max_length = 10 , choices = ROLE_CHOICES)
+    def __str__(self):
+        return f"{self.username} ({self.role})"
+    
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+from .serializers import UserRegisterSerializer
 
-# Create your views here.
+# Register
+class RegisterView(generics.CreateAPIView):
+    serializer_class = UserRegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+# Login
+class LoginView(generics.GenericAPIView):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+                'username': user.username,
+                'role': user.role
+            })
+        return Response({'error': 'Invalid Credentials'}, status=400)
+
+# Logout (Blacklist refresh token)
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.views import APIView
+
+class LogoutView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()  # blacklist the token
+            return Response(status=205)
+        except Exception as e:
+            return Response(status=400)
